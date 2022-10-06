eb414bd8c1d6811bc97d9c67bc614c7b4ed7e6dd
erenGuden <105981672+erenGuden@users.noreply.github.com>
2022-10-06 16:37:42 -0700

    fix error

--- src/MovieBox.js
+++ src/MovieBox.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Modal from "./Modal";
+// import Modal from "./Modal";
 
 const IMG = "https://image.tmdb.org/t/p/w500";
 const MovieBox = ({ movie }) => {
