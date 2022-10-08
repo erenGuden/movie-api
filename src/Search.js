7235f478916ea38ca5fcddae20839dfc2062945a
erenGuden <105981672+erenGuden@users.noreply.github.com>
2022-10-07 17:09:39 -0700

    add debounceInput from react
    Add debounce function for search component

--- src/Search.js
+++ src/Search.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
+import { DebounceInput } from "react-debounce-input";
 
 const Search = ({ getQuery }) => {
   const [text, setText] = useState("");
-
 
   const onChange = (q) => {
     setText(q);
     getQuery(q);
   };
-  
+
   return (
     <section className="search-class">
       <form>
-        <input
+        <DebounceInput
+          debounceTimeout={400}
           type="text"
           className="form-control"
           placeholder="Search movies"
