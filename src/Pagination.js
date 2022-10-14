59137dc6a04721341a4636a69e5909ffe3463240
erenGuden <105981672+erenGuden@users.noreply.github.com>
2022-10-13 22:38:24 -0700

    pagine-search fix
    Add prevent page refresh for both paginate and search functions.
    Add paginate-search function.

--- src/Pagination.js
+++ src/Pagination.js
@@ -6,28 +6,28 @@ const Pagination = ({ currentPage, onClick }) => {
       <ul className="pagination-class">
         <li className="page-item-class">
         <a
-            onClick={() => onClick(1)}
+            onClick={(e) => onClick(e, 1)}
             href="!#"
             className="page-link-class"
           >
             &lt;&lt;
           </a>
           <a
-            onClick={() => onClick(currentPage - 1)}
+            onClick={(e) => onClick(e, currentPage - 1)}
             href="!#"
             className="page-link-class"
           >
             &lt;
           </a>
           <a
-            onClick={() => onClick(currentPage + 1)}
+            onClick={(e) => onClick(e, currentPage + 1)}
             href="!#"
             className="page-link-class"
           >
             &gt;
           </a>
            <a
-            onClick={() => onClick()}
+            onClick={(e) => onClick(e)}
             href="!#"
             className="page-link-class"
           >
