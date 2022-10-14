49f050f6153a95e48b5d0fa2d3d7497199c73dee
erenGuden <105981672+erenGuden@users.noreply.github.com>
2022-10-13 21:16:13 -0700

    Bug fix
    Bug fix resolved for the search text deleted, it did not show any movies.

--- src/App.js
+++ src/App.js
@@ -5,35 +5,40 @@ import axios from "axios";
 import MovieGrid from "./MovieGrid";
 import Search from "./Search";
 import Pagination from "./Pagination";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [query, setQuery] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
-  const totalPages = 500
+  const totalPages = 500;
   const apiKey = "44c327a909613dc46bc70a8c7e59ea18";
 
   useEffect(() => {
-    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&query&page=${currentPage}`;
-    const fetchMovies = async () => {
-      const result = await axios(url);
-      setMovies(result.data.results);
-    };
-    fetchMovies();
+    getPopularMovies();
   }, [currentPage]);
 
   useEffect(() => {
     const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=`;
     const searchMovies = async () => {
-      if (!query) return;
+      if (!query) return getPopularMovies();
       const searchResult = await axios(searchUrl + query);
       setMovies(searchResult.data.results);
     };
     searchMovies();
   }, [query]);
 
-  const handlePageChange = (pageNumber = totalPages) => setCurrentPage(pageNumber);
+  const getPopularMovies = () => {
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&query&page=${currentPage}`;
+    const fetchMovies = async () => {
+      const result = await axios(url);
+      setMovies(result.data.results);
+    };
+    fetchMovies();
+  };
+
+  const handlePageChange = (pageNumber = totalPages) =>
+    setCurrentPage(pageNumber);
 
   return (
     <div className="main-class">
@@ -41,7 +46,7 @@ function App() {
         <Header />
         <Search getQuery={(q) => setQuery(q)} />
         <MovieGrid movies={movies} />
-        <Pagination currentPage={currentPage} onClick={handlePageChange}/>
+        <Pagination currentPage={currentPage} onClick={handlePageChange} />
       </div>
     </div>
   );
